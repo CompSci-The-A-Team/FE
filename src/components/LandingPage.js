688e966e55886aa9c80f71934c2c17d7ec370992
Sydney Blom <sydneyblom@#gmail.com>
2020-03-04 17:53:34 -0600

    changed

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -3,6 +3,8 @@ import { NavLink } from "react-router-dom";
 import { Button } from "semantic-ui-react";
 
 class LandingPage extends React.Component {
+
+  
   render() {
     return (
       <div className="background-image">
