029358c6d503bd3dcb19826c3518d4b3d990c1e9
Sydney Blom <sydneyblom@#gmail.com>
2020-03-04 14:18:51 -0600

    redid routes a bit now working on restyling

--- src/App.js
+++ src/App.js
@@ -1,43 +1,25 @@
 import React from "react";
-import { Route, NavLink, withRouter } from "react-router-dom";
+import { BrowserRouter as Router,  Route, withRouter } from "react-router-dom";
 import Login from "./components/Login";
+import WelcomeScreen from "./components/WelcomeScreen"
 import Register from "./components/Register";
+import LandingPage from "./components/LandingPage"
 import "./App.css";
-import { Button } from "semantic-ui-react";
 
 class App extends React.Component {
   render() {
     return (
-      <div className="background-image">
+      <Router>
         <div className="App">
-          <header>
-            <div className="title">
-              <h1>Enter The Ocean Game</h1>
-            </div>
-          </header>
-          <div className="startButtons">
-            <Button>
-              <NavLink className="navLink" to="/register">
-                New Player?
-              </NavLink>
-            </Button>
-
-            <Button>
-        
-              <NavLink className="navLink" to="/login">
-                Existing Player
-              </NavLink>
-            </Button>
-          </div>
-
-          <div>
-            <Route path="/register" component={Register} />
-            <Route path="/login" component={Login} />
-          </div>
+        <Route exact path="/register" component={Register} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/welcome" component={WelcomeScreen} />
+        <Route exact path="/" component={LandingPage} />
         </div>
-      </div>
+      </Router>
     );
   }
 }
 
 export default withRouter(App);
+
---
+++ src/components/WelcomeScreen.js
@@ -0,0 +1,16 @@
+import React from "react";
+import {Button} from 'semantic-ui-react';
+
+const WelcomeScreen = () => {
+    return (
+      <div className="WelcomeScreen">
+
+          <h1>Welcome</h1>
+          <p>The story of the game here</p>
+<h1>How to Play</h1>
+<p>The directions of the game here</p>
+<Button> Let's Play</Button>
+      </div>
+    )
+}
+export default WelcomeScreen;
