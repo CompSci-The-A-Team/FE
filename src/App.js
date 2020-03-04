a3198a2ab66d971a7cfd5fc4e2164cb04bde2c56
Sydney Blom <sydneyblom@#gmail.com>
2020-03-04 10:35:36 -0600

    frontend css

--- src/App.js
+++ src/App.js
@@ -1,30 +1,39 @@
-import React from 'react';
-import { Route, NavLink, withRouter } from 'react-router-dom';
-import Login from './components/Login';
-import Register from './components/Register';
-import './App.css';
-import { Button } from 'semantic-ui-react';
+import React from "react";
+import { Route, NavLink, withRouter } from "react-router-dom";
+import Login from "./components/Login";
+import Register from "./components/Register";
+import "./App.css";
+import { Button } from "semantic-ui-react";
 
 class App extends React.Component {
-
   render() {
     return (
-      <div className="App">
-        <header>
-          <nav>
-          <div className='title'>
-          <h1>Enter The Ocean Game</h1>
-        </div>
-              <Button ><NavLink className='navLink' to='/register'>New Player?</NavLink></Button>
-              <Button > <NavLink className='navLink' to='/login'>Existing Player</NavLink></Button>
-          </nav>
-        </header>
+      <div className="background-image">
+        <div className="App">
+          <header>
+            <div className="title">
+              <h1>Enter The Ocean Game</h1>
+            </div>
+          </header>
+          <div className="startButtons">
+            <Button>
+              <NavLink className="navLink" to="/register">
+                New Player?
+              </NavLink>
+            </Button>
 
+            <Button>
+        
+              <NavLink className="navLink" to="/login">
+                Existing Player
+              </NavLink>
+            </Button>
+          </div>
 
-
-        <div>
-          <Route path='/register' component={Register} />
-          <Route path='/login' component={Login} />
+          <div>
+            <Route path="/register" component={Register} />
+            <Route path="/login" component={Login} />
+          </div>
         </div>
       </div>
     );
